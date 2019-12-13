151273f01491da240a722bb03fe77cadb6557828
ltj <a121bc@163.com>
2019-12-13 14:13:45 +0800

    添加安全网关

---
+++ security-gateway/src/main/java/com/ltj/gateway/SecurityGatewayApplication.java
@@ -0,0 +1,32 @@
+package com.ltj.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @describe： TODO
+ * @author: Liu Tian Jun
+ * @Date: 2019-12-13 13:13
+ * @version: 1.0
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SecurityGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityGatewayApplication.class, args);
+    }
+
+    @RequestMapping("/api")
+    @RestController
+    public class ApiTest {
+
+        @GetMapping("/demo")
+        private String  demo(){
+            return "demo";
+        }
+    }
+}
---
+++ security-gateway/src/main/java/com/ltj/gateway/handler/AuthenticationSuccessHandler.java
@@ -0,0 +1,77 @@
+package com.ltj.gateway.handler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.JsonObject;
+import com.ltj.gateway.entity.AuthUserDetails;
+import com.ltj.gateway.response.MessageCode;
+import com.ltj.gateway.response.WsResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.server.WebFilterExchange;
+import org.springframework.security.web.server.authentication.WebFilterChainServerAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.Base64;
+
+
+@Component
+public class AuthenticationSuccessHandler extends WebFilterChainServerAuthenticationSuccessHandler   {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public Mono<Void> onAuthenticationSuccess(WebFilterExchange webFilterExchange, Authentication authentication){
+        ServerWebExchange exchange = webFilterExchange.getExchange();
+        ServerHttpResponse response = exchange.getResponse();
+        //设置headers
+        HttpHeaders httpHeaders = response.getHeaders();
+        httpHeaders.add("Content-Type", "application/json; charset=UTF-8");
+        httpHeaders.add("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0");
+        //设置body
+        WsResponse wsResponse = WsResponse.success();
+       byte[]   dataBytes={};
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            User user=(User)authentication.getPrincipal();
+            AuthUserDetails userDetails=buildUser(user);
+            byte[] authorization=(userDetails.getUsername()+":"+userDetails.getPassword()).getBytes();
+            String token= Base64.getEncoder().encodeToString(authorization);
+            httpHeaders.add(HttpHeaders.AUTHORIZATION, token);
+            wsResponse.setResult(userDetails);
+            dataBytes=mapper.writeValueAsBytes(wsResponse);
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+            JsonObject result = new JsonObject();
+            result.addProperty("status", MessageCode.COMMON_FAILURE.getCode());
+            result.addProperty("message", "授权异常");
+            dataBytes=result.toString().getBytes();
+        }
+        DataBuffer bodyDataBuffer = response.bufferFactory().wrap(dataBytes);
+        return response.writeWith(Mono.just(bodyDataBuffer));
+    }
+
+
+
+    private AuthUserDetails buildUser(User user){
+        AuthUserDetails userDetails=new AuthUserDetails();
+        userDetails.setUsername(user.getUsername());
+        userDetails.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userDetails;
+    }
+
+
+
+
+
+
+
+}
